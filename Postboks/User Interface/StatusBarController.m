4c623a9c1756918f80899d2d377665bd70baf85b
Jonas Boserup <jonas@iboserup.dk>
2016-01-07 16:14:25 +0100

    Indentation

--- Postboks/User Interface/StatusBarController.m
+++ Postboks/User Interface/StatusBarController.m
@@ -51,7 +51,7 @@
 	self.statusItem.alternateImage = [NSImage imageNamed:@"menubar_icon_selected"];
 	self.statusItem.title = @"";
 	self.statusItem.highlightMode = YES;
-    [self.statusItem.image setTemplate:YES];
+	[self.statusItem.image setTemplate:YES];
 
 	NSMenu *menu = [NSMenu new];
 	[menu setAutoenablesItems:NO];
